a2cb2834a8a9873d78a0eb12ca6fbe27f720d0e0
stuart-rickard <stuart@bau-dev.com>
2022-03-25 13:43:06 -0700

    updated Nav test

--- src/components/Nav/__tests__/index.tests.js
+++ src/components/Nav/__tests__/index.tests.js
@@ -2,12 +2,23 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
+const categories = [
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
 
 afterEach(cleanup);
 
 describe("Nav component", () => {
   it("renders", () => {
-    render(<Nav />);
+    render(
+      <Nav
+        categories={categories}
+        mockSetCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
   });
   it("matches snapshot", () => {
     const { asFragment } = render(<Nav />);
